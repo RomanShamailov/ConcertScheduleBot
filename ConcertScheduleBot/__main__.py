2bf73dd31d2b81f12f5662ae2cdcdedef28020be
Roman Shamailov <rgshamailov@edu.hse.ru>
2025-12-24 17:06:24 +0300

    added image

--- ConcertScheduleBot/__main__.py
+++ ConcertScheduleBot/__main__.py
@@ -13,7 +13,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["BOT_TOKEN"] = "TEST_TOKEN"
 TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
     raise RuntimeError("BOT_TOKEN is not set")
